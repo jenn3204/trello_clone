da0317e0de8730ebe6553a02f7111392f63bc9e7
jenniferjaque <jenn3204@stud.kea.dk>
2020-04-21 20:39:16 +0200

    moving left and right

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ window.addEventListener("DOMContentLoaded", start);
 const endpoint = "https://frontendjenn-13a8.restdb.io/rest/trelloitems";
 const apiKey = "5e957ab7436377171a0c2342";
 
-const form = document.querySelector("form");
-const elements = form.elements;
+const addForm = document.querySelector("#add_form");
+const editForm = document.querySelector("#edit_form");
+
+const elements = addForm.elements;
+const editElements = editForm.elements;
+
+const todoList = document.querySelector("#todo");
+const doingList = document.querySelector("#doing");
+const doneList = document.querySelector("#done");
 
 function start() {
-  //   elements.color.addEventListener("click", (e) => {
-  //     console.log(elements.color.value);
-  //     document.querySelector("body").style.background = elements.color.value;
-  //   });
   get();
 
-  document.querySelector("form").addEventListener("submit", (event) => {
+  addForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let data = {
@@ -28,17 +31,17 @@ function start() {
       list: elements.radio.value,
     };
 
-    console.log(data);
+    console.log("første data:" + data);
 
     post(data);
   });
 
   document.querySelector("#showform").addEventListener("click", () => {
-    form.classList.toggle("hide");
+    addForm.classList.toggle("hide");
     document.querySelector("#showform").textContent = "-";
-    if (form.classList.contains("hide")) {
+    if (addForm.classList.contains("hide")) {
       document.querySelector("#showform").textContent = "+";
-      form.reset();
+      addForm.reset();
     }
   });
 }
@@ -63,9 +66,6 @@ function showCards(data) {
 function showCard(card) {
   const cardTemplate = document.querySelector("#card").content;
   const cardclone = cardTemplate.cloneNode(true);
-  const todoList = document.querySelector("#todo");
-  const doingList = document.querySelector("#doing");
-  const doneList = document.querySelector("#done");
 
   cardclone.querySelector("article").dataset.id = card._id;
   cardclone.querySelector("h4").textContent = card.title;
@@ -73,6 +73,9 @@ function showCard(card) {
   cardclone.querySelector("article").style.background = card.color;
 
   cardclone.querySelector(".deletebutton").addEventListener("click", () => deleteIt(card._id));
+  cardclone.querySelector(".edit").addEventListener("click", () => showPopup(card, card._id));
+  cardclone.querySelector(".right").addEventListener("click", () => right(card, card._id));
+  cardclone.querySelector(".left").addEventListener("click", () => left(card, card._id));
 
   console.log(card.list);
 
@@ -87,6 +90,36 @@ function showCard(card) {
   if (card.list == "done") {
     doneList.appendChild(cardclone);
   }
+}
+
+function showPopup(card, id) {
+  console.log("show popup");
+  document.querySelector("#popup_form").classList.remove("hide");
+
+  console.log("id" + id);
+
+  editElements.title.value = card.title;
+  editElements.desc.value = card.desc;
+  editElements.creator.value = card.creator;
+  editElements.deadline.value = card.deadline;
+  editElements.estimate.value = card.est;
+  editElements.radio.value = card.list;
+  editElements.color.value = card.color;
+
+  document.querySelector("#close").addEventListener("click", () => {
+    document.querySelector("#popup_form").classList.add("hide");
+    editForm.classList = "";
+  });
+
+  editForm.addEventListener("submit", () => submitEdit(event, id, card));
+}
+
+function submitEdit(event, id, card) {
+  event.preventDefault();
+  document.querySelector("#popup_form").classList.add("hide");
+  console.log("nu id" + id);
+
+  put(id, card);
 }
 
 function post(data) {
@@ -119,4 +152,71 @@ function deleteIt(id) {
   })
     .then((res) => res.json())
     .then((data) => console.log(data));
+}
+
+function put(id, card) {
+  const newData = {
+    title: editElements.title.value,
+    desc: editElements.desc.value,
+    est: editElements.estimate.value,
+    deadline: editElements.deadline.value,
+    color: editElements.color.value,
+    creator: editElements.creator.value,
+    list: editElements.radio.value,
+  };
+
+  console.log("title:" + editElements.title.value);
+
+  let postData = JSON.stringify(newData);
+
+  fetch(`${endpoint}/${id}`, {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": apiKey,
+      "cache-control": "no-cache",
+    },
+    body: postData,
+  })
+    .then((d) => d.json())
+    .then((newData) => {
+      let newclone = document.querySelector(`article[data-id="${id}"]`);
+      newclone.remove();
+      console.log(newclone);
+      console.log(id);
+      newclone.querySelector("h4").textContent = newData.title;
+      newclone.querySelector("p").textContent = newData.desc;
+      newclone.style.background = newData.color;
+      document.querySelector(`#${newData.list}`).appendChild(newclone);
+    });
+}
+
+function right(card, id) {
+  console.log(card);
+
+  let cardclone = document.querySelector(`article[data-id="${id}"]`);
+
+  if (card.list == "todo") {
+    doingList.appendChild(cardclone);
+    card.list = "done";
+  } else if (card.list == "doing") {
+    doneList.appendChild(cardclone);
+  } else if (card.list == "done") {
+    doneList.appendChild(cardclone);
+  }
+}
+
+function left(card, id) {
+  let cardclone = document.querySelector(`article[data-id="${id}"]`);
+  console.log(card.list);
+
+  if (card.list == "todo") {
+    todoList.appendChild(cardclone);
+  } else if (card.list == "doing") {
+    todoList.appendChild(cardclone);
+    card.list = "todo";
+  } else if (card.list == "done") {
+    doingList.appendChild(cardclone);
+    card.list = "doing";
+  }
 }
